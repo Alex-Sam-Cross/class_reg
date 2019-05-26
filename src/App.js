afc329cb271c976294367a04749152bfe89dd3e2
Sam Cross <samcross@192.168.1.8>
2019-05-26 16:23:47 +0200

    creation of repo and readMe

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './css/App.scss';
+
+class App extends Component {
+  // constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // set states
+    };
+    //binds
+  }
+
+  // methods
+
+  // render
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default App;
